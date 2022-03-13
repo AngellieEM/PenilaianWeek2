0e67b09850c72b07dc397a07c50c97d73e1968d5
Angellie Ennovaryn M <aennovaryn@student.ciputra.ac.id>
2022-03-13 22:33:50 +0700

    Add project files.

---
+++ PenilaianWeek2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace PenilaianWeek2
+{
+    partial class FormPengaturanWarna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelInputData = new System.Windows.Forms.Label();
+            this.TextBoxInputData = new System.Windows.Forms.TextBox();
+            this.ButtonProses = new System.Windows.Forms.Button();
+            this.LabelOutputData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LabelInputData
+            // 
+            this.LabelInputData.AutoSize = true;
+            this.LabelInputData.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelInputData.Location = new System.Drawing.Point(30, 38);
+            this.LabelInputData.Name = "LabelInputData";
+            this.LabelInputData.Size = new System.Drawing.Size(76, 19);
+            this.LabelInputData.TabIndex = 0;
+            this.LabelInputData.Text = "Input Data:";
+            // 
+            // TextBoxInputData
+            // 
+            this.TextBoxInputData.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxInputData.Location = new System.Drawing.Point(34, 71);
+            this.TextBoxInputData.Name = "TextBoxInputData";
+            this.TextBoxInputData.Size = new System.Drawing.Size(343, 26);
+            this.TextBoxInputData.TabIndex = 1;
+            // 
+            // ButtonProses
+            // 
+            this.ButtonProses.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonProses.Location = new System.Drawing.Point(34, 115);
+            this.ButtonProses.Name = "ButtonProses";
+            this.ButtonProses.Size = new System.Drawing.Size(75, 23);
+            this.ButtonProses.TabIndex = 2;
+            this.ButtonProses.Text = "Proses";
+            this.ButtonProses.UseVisualStyleBackColor = true;
+            this.ButtonProses.Click += new System.EventHandler(this.ButtonProses_Click);
+            // 
+            // LabelOutputData
+            // 
+            this.LabelOutputData.AutoSize = true;
+            this.LabelOutputData.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelOutputData.Location = new System.Drawing.Point(28, 154);
+            this.LabelOutputData.Name = "LabelOutputData";
+            this.LabelOutputData.Size = new System.Drawing.Size(123, 31);
+            this.LabelOutputData.TabIndex = 3;
+            this.LabelOutputData.Text = "[EMPTY]";
+            // 
+            // FormPengaturanWarna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(472, 267);
+            this.Controls.Add(this.LabelOutputData);
+            this.Controls.Add(this.ButtonProses);
+            this.Controls.Add(this.TextBoxInputData);
+            this.Controls.Add(this.LabelInputData);
+            this.Name = "FormPengaturanWarna";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelInputData;
+        private System.Windows.Forms.TextBox TextBoxInputData;
+        private System.Windows.Forms.Button ButtonProses;
+        private System.Windows.Forms.Label LabelOutputData;
+    }
+}
+
---
+++ PenilaianWeek2/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PenilaianWeek2
+{
+    public partial class FormPengaturanWarna : Form
+    {
+        public FormPengaturanWarna()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonProses_Click(object sender, EventArgs e)
+        {
+            sistemKerja();
+        }
+        public void sistemKerja()
+        {
+            //ketentuan isi
+            string FirstTry = "";
+            FirstTry = TextBoxInputData.Text;
+            string[] LastText = FirstTry.Split(':');
+            if (FirstTry.Contains(':'))
+            {
+                LastText[0] = LastText[0].ToUpper();
+                LastText[1] = LastText[1].ToUpper();
+            }
+            else
+            {
+                FirstTry = FirstTry.ToUpper();
+            }
+            if (LastText[0] == "ISI")
+            {
+                string text = "";
+                text = TextBoxInputData.Text;
+                string[] TextAkhir = text.Split(':');
+                LabelOutputData.Text = TextAkhir[1];
+            }
+            else if (FirstTry == "SHOWN")
+            {
+                //shown option
+                LabelOutputData.Show();
+            }
+            else if (FirstTry == "HIDE")
+            {
+                //hide option
+                LabelOutputData.Hide();
+            }
+            else if (LastText[0] == "WARNA")
+            {
+                //changing the text's colour : Defaul (hitam), Merah, Hijau, Biru, Kuning, Ungu, Jingga
+                if (LastText[1].Contains("DEFAULT"))
+                {
+                    LabelOutputData.ForeColor = Color.Black;
+                }
+                else if (LastText[1].Contains("MERAH"))
+                {
+                    LabelOutputData.ForeColor = Color.Red;
+                }
+                else if (LastText[1].Contains("HIJAU"))
+                {
+                    LabelOutputData.ForeColor = Color.Green;
+                }
+                else if (LastText[1].Contains("BIRU"))
+                {
+                    LabelOutputData.ForeColor = Color.Blue;
+                }
+                else if (LastText[1].Contains("KUNING"))
+                {
+                    LabelOutputData.ForeColor = Color.Yellow;
+                }
+                else if (LastText[1].Contains("UNGU"))
+                {
+                    LabelOutputData.ForeColor = Color.Purple;
+                }
+                else if (LastText[1].Contains("JINGGA"))
+                {
+                    LabelOutputData.ForeColor = Color.Orange;
+                }
+            }
+            else if (FirstTry == "RESTART")
+            {
+                //restart option
+                LabelOutputData.Text = "[EMPTY]";
+                LabelOutputData.ForeColor = Color.Black;
+                LabelOutputData.Font = new Font("Times New Rowman", 12F);
+            }
+            else if (FirstTry == "BESARKAN")
+            {
+                //make the santences or word bigger
+                LabelOutputData.Font = ChangeFontSize(LabelOutputData.Font, LabelOutputData.Font.Size + 3);
+            }
+            else if (FirstTry == "KECILKAN")
+            {
+                //make the santences or word smaller
+                LabelOutputData.Font = ChangeFontSize(LabelOutputData.Font, LabelOutputData.Font.Size - 3);
+            }
+            else
+            {
+                //showing the maessage box 
+                MessageBox.Show("Syntax tidak terdaftar!");
+            }
+        }
+        static public Font ChangeFontSize(Font font, float fontSize)
+        {
+            if (font != null)
+            {
+                //for changing font size
+                float currentSize = font.Size;
+                if (currentSize != fontSize)
+                {
+                    font = new Font(font.Name, fontSize, font.Style, font.Unit, font.GdiCharSet, font.GdiVerticalFont);
+                }
+            }
+            return font;
+        }
+    }
+}
